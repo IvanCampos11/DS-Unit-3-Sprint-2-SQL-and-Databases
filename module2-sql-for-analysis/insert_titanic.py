3005c4b6c0d7b5fd2910a6cfad6c3741f5223389
IvanCampos11 <39417756+IvanCampos11@users.noreply.github.com>
2020-08-11 19:54:49 -0400

    A bunch of stuff

--- module2-sql-for-analysis/insert_titanic.py
+++ module2-sql-for-analysis/insert_titanic.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import psycopg2
 
-df = pd.read_csv('titanic.csv')
+titanic = pd.read_csv('titanic.csv').rename({
+    "Siblings/Spouces Aboard": "Siblings_Spouses_Aboard",
+    "Parents/Children Aboard": "Parents_Children_Aboard",
+}, axis=1)
 
+titanic['Name'] = titanic['Name'].str.replace("'", "")
 
 dbname = 'ksevjtjp'
 user = 'ksevjtjp'
@@ -12,3 +16,30 @@ host = 'isilo.db.elephantsql.com'
 pg_conn = psycopg2.connect(dbname=dbname, user=user,
                            password=password, host=host)
 pg_curs = pg_conn.cursor()
+
+create_titanic_table = """
+DROP TABLE IF EXISTS titanic;
+CREATE TABLE titanic (
+  Survived INT,
+  Pclass INT,
+  Name VARCHAR,
+  Sex VARCHAR,
+  Age FLOAT,
+  Siblings_Spouses_Aboard INT,
+  Parents_Children_Aboard INT,
+  Fare FLOAT
+);
+"""
+
+if __name__ == "__main__":
+    pg_curs.execute(create_titanic_table)
+    pg_conn.commit()
+    for character in titanic:
+        insert_character = """
+          INSERT INTO charactercreator_character
+          (Survived, Pclass, Name, Sex, Age, Siblings_Spouses_Aboard, Parents_Children_Aboard)
+          VALUES """ + str(titanic[1:]) + ";"
+        pg_curs.execute(insert_character)
+    pg_conn.commit()
+    pg_curs.close()
+    pg_conn.close()
